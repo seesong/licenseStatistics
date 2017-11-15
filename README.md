7d2eed0aa3ef0f4927177ebec686c8b707d540b0
sunny <qi.cao@cloudminds.com>
2017-11-15 14:00:43 +0800

    :zap:完成License统计。 :books:文档书写。

--- README.md
+++ README.md
@@ -9,4 +9,23 @@
 
 ## 功能一：电表数量统计
 
-## 功能二：电表详情cvs表格输出                                                             
+主要是学习学习MongoDB的distinct。
+
+
+	
+
+	db.elemeter.distinct("device_id")
+
+## 功能二：电表详情cvs表格输出          
+
+https://stackoverflow.com/questions/26062658/mongodb-aggregation-in-golang
+
+	db.elemeter.aggregate({"$group":
+                        {"_id":{"device_id":"$device_id"},
+                         "pubtime":{"$last": "$pubtime"} 
+                         }});
+这个方法有些问题，还不知道怎么解决。                       
+
+	http://www.01happy.com/golang-mongodb-find-demo/
+	https://stackoverflow.com/questions/26062658/mongodb-aggregation-in-golang
+	https://godoc.org/labix.org/v2/mgo#Collection.Pipe                            
--- licenseStatistics.go
+++ licenseStatistics.go
@@ -13,6 +13,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/sunnyregion/color"
 	"github.com/sunnyregion/sunnyini"
@@ -24,6 +25,90 @@ var (
 	sumFlag = flag.Bool("sum", false, "是否统计有多少块电表在统计。")
 )
 
+//对象化
+type LicenseStuct struct {
+	err     error
+	MongoDB *mgo.Database
+}
+
+//数据库结构
+type ElemeterMgo struct {
+	ImageFile string    `bson:"image_file"`
+	DeviceId  string    `bson:"device_id"`
+	Type      string    `bson:"type"`
+	SubType   string    `bson:"sub_type"`
+	Value     string    `bson:"value"`
+	Flag      int       `bson:"flag"`
+	IP        string    `bson:"ipaddress"`
+	Pubtime   time.Time `bson:"pubtime"`
+}
+
+var (
+	dbhostsip  = "127.0.0.1" //IP地址
+	port       = "27017"
+	dbusername = ""         //用户名
+	dbpassword = ""         //密码
+	database   = "elemeter" //表名
+	collection = ""
+)
+
+func NewLicenseStuct() *LicenseStuct {
+
+	f := sunnyini.NewIniFile()
+	f.Readfile("config.ini")
+	describ, v := f.GetValue("mongodb")
+	if describ == "" { // 有数据
+		dbhostsip = v[0]["dbhostsip"] //IP地址
+		port = v[1]["port"]
+		dbusername = v[2]["dbusername"] //用户名
+		dbpassword = v[3]["dbpassword"] //密码
+		database = v[4]["dbname"]       //表名
+		collection = v[5]["collection"]
+	} else {
+		fmt.Println(describ)
+	}
+	Host := []string{
+		dbhostsip + ":" + port,
+		// replica set addrs...
+	}
+
+	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs:    Host,
+		Timeout:  3 * time.Second, //10秒连接不到数据库
+		Username: dbusername,
+		Password: dbpassword,
+		// Database: Database,
+		// DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
+		// 	return tls.Dial("tcp", addr.String(), &tls.Config{})
+		// },
+	})
+	if err != nil {
+		result := &LicenseStuct{err: err, MongoDB: nil}
+		return result
+	}
+	result := &LicenseStuct{err: nil, MongoDB: session.DB(database)}
+	return result
+}
+
+// 统计电表数量
+func (this *LicenseStuct) SumLicense() {
+	color := color.New(color.FgHiGreen).Add(color.BgBlack)
+	c := this.MongoDB.C(collection)
+	//	_ = c
+	//	o1 := bson.M{"_id": bson.M{"device_id": "$device_id"}}
+
+	//	o2 := bson.M{"pubtime": bson.M{"$last": "$pubtime"}}
+	//	o3 := bson.M{"$group": []bson.M{o1, o2}}
+	//	pipe := c.Pipe([]bson.M{o3})
+	//	var result []bson.M{}
+	//	_ = pipe.All(&result)
+	//	fmt.Println(result)
+	var results []interface{}
+	//c.Find(nil).Select(bson.M{"device_id": 1, "pubtime": 1}).Distinct("device_id", &results)
+	c.Find(nil).Distinct("device_id", &results)
+	color.Println("电表数量：", len(results))
+	return
+}
 func init() {
 	c := color.New(color.FgHiMagenta).Add(color.BgBlack)
 	c.Println(`
@@ -36,6 +121,7 @@ func init() {
                                                                                             
 	`)
 	flag.Parse()
+
 }
 
 //统计电报数量
@@ -45,9 +131,14 @@ func sumLicense() (result int) {
 
 //主程序
 func main() {
-
+	c := color.New(color.FgHiMagenta).Add(color.BgBlack)
 	if *sumFlag {
-		fmt.Println("ok")
+		l := NewLicenseStuct()
+		if l.err == nil {
+			l.SumLicense()
+		} else {
+			c.Println(l.err.Error())
+		}
 	}
 
 }
